8f33797cbb373c75bb673455120aa1548aea5219
Geekboots <info@geekboots.com>
2024-04-15 21:59:08 +0530

    db uri removed

--- config/default.js
+++ config/default.js
@@ -1,6 +1,5 @@
 module.exports = {
   port: 8000,
-  dbURI:
-    "mongodb+srv://geekboots:oQxbdHvFYVdx0tuB@geekboots.2wm4gel.mongodb.net/userAuth?retryWrites=true&w=majority&appName=Geekboots",
+  dbURI: "",
   saltFactor: 12,
 };
